812d8d1a502dd27e00e0e5613c81a512812a87e8
3drdProgramming <programming@3droboticsduluth.com>
2026-08-29 06:11:35 -0500

    Add neutral TeamCode seasonal scaffold

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.kt
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.opmodes
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+
+@Autonomous
+@Suppress("unused")
+class Auto : OpMode()
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Config.kt
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.subsystems
+
+import com.bylazar.configurables.annotations.Configurable
+import org.firstinspires.ftc.threedrd.nextftc.config.Diagnostics.Level
+import org.firstinspires.ftc.threedrd.nextftc.config.Diagnostics.Level.INFO
+import org.firstinspires.ftc.threedrd.nextftc.config.Setting
+import org.firstinspires.ftc.threedrd.nextftc.subsystems.ConfigSubsystem
+
+@Configurable
+object Config : ConfigSubsystem() {
+    override var config = Config()
+        private set
+
+    data class Config(
+        @Setting(live = true)
+        var level: Level = INFO,
+
+        @Transient
+        var filter: String = ""
+    )
+}
