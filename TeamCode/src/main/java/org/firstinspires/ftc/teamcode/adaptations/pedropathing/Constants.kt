29075d0966e658726f8d1435d09150b8949fabcc
3drdProgramming <programming@3droboticsduluth.com>
2026-08-29 06:11:35 -0500

    Add neutral TeamCode seasonal scaffold

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/adaptations/pedropathing/Constants.kt
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.adaptations.pedropathing
+
+import com.pedropathing.follower.Follower
+import com.pedropathing.follower.FollowerConstants
+import com.pedropathing.ftc.FollowerBuilder
+import com.pedropathing.ftc.drivetrains.MecanumConstants
+import com.pedropathing.ftc.localization.constants.PinpointConstants
+import com.pedropathing.paths.PathConstraints
+import com.qualcomm.robotcore.hardware.HardwareMap
+import dev.nextftc.core.units.inches
+import kotlin.math.max
+
+object Constants {
+    // Template dimensions and Pedro defaults; replace these when starting a robot season.
+    val robotLength = 18.inches
+    val robotWidth = 18.inches
+    val robotRadius = max(robotLength.inIn, robotWidth.inIn) / 2
+
+    var followerConstants = FollowerConstants()
+    var pathConstraints = PathConstraints.defaultConstraints
+    var driveConstants = MecanumConstants()
+    var localizerConstants = PinpointConstants()
+
+    fun createFollower(hardwareMap: HardwareMap): Follower =
+        FollowerBuilder(followerConstants, hardwareMap)
+            .pathConstraints(pathConstraints)
+            .mecanumDrivetrain(driveConstants)
+            .pinpointLocalizer(localizerConstants)
+            .build()
+}
