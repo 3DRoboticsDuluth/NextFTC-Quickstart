ca479cd13321a56fe0b6aafc47128f6fdf0a7063
3drdProgramming <programming@3droboticsduluth.com>
2026-09-01 17:37:25 -0500

    Add a neutral autonomous example

--- TeamCode/src/test/java/org/firstinspires/ftc/teamcode/opmodes/AutoTests.kt
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/opmodes/AutoTests.kt
@@ -1,11 +1,28 @@
 package org.firstinspires.ftc.teamcode.opmodes
 
+import dev.nextftc.core.commands.CommandManager
+import org.firstinspires.ftc.teamcode.subsystems.Auto.execute
+import org.junit.After
 import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class AutoTests {
+    @After
+    fun tearDown() {
+        CommandManager.cancelAll()
+        CommandManager.run()
+    }
+
     @Test
     fun compositionCanBeCreated() {
         assertNotNull(Auto())
+    }
+
+    @Test
+    fun schedulesTheExampleRoutineAtStart() {
+        Auto().onStartButtonPressed()
+
+        assertTrue(execute.isScheduled)
     }
 }
